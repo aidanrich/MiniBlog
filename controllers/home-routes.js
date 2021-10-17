4b4fba8a032dbbff07db805ee846f10fe8495c45
aorich <rich.aidan@gmail.com>
2021-10-17 13:25:02 -0400

    inching along

---
+++ controllers/home-routes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { Blog } = require('../models');
+
+// Get all blog entries
+router.get('/', async (req, res) => {
+    try {
+        const blogData = await Blog.findAll();
+        
+        const blogAll = blogData.map((blog) => blog.get({ plain: true }));
+        res.render('all', {blogAll});
+    
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+});
---
+++ models/Blog.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content:{
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blog',
+  }
+);
+
+module.exports = Blog;
