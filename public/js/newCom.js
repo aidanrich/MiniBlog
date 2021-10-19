10cf7d57e7132363733783dd7e9efe5cdc06d947
aorich <rich.aidan@gmail.com>
2021-10-19 10:00:59 -0400

    bug fixed

--- public/js/newCom.js
+++ public/js/newCom.js
@@ -4,15 +4,16 @@ const newCommentFunc = async (event) => {
   console.log("comment hit 2")
 
   const mess_content = document.querySelector('.blog-comment').value.trim();
-  const blog_id = document.querySelector('.blog-num').value.trim();
+  const blog_id = document.querySelector('.blog-num');
 
   console.log(blog_id)
+  console.log(mess_content)
 
-  if (mess_content) {
+  if (mess_content && blog_id) {
 
     const response = await fetch(`/api/comments`, {
       method: 'POST',
-      body: JSON.stringify({ mess_content }),
+      body: JSON.stringify({ mess_content, blog_id }),
       headers: {
         'Content-Type': 'application/json',
       },
@@ -22,7 +23,7 @@ const newCommentFunc = async (event) => {
 
       document.location.replace('/');
     } else {
-      alert('Failed to post blog');
+      alert('Failed to post comment');
     }
   }
 }
