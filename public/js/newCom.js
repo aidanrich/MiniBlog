d92633161688876bc66ce2e71c60253b3d8e3833
aorich <rich.aidan@gmail.com>
2021-10-19 15:09:30 -0400

    add user works

--- public/js/newCom.js
+++ public/js/newCom.js
@@ -1,33 +1,33 @@
-const newCommentFunc = async (event) => {
-  event.preventDefault();
+// const newCommentFunc = async (event) => {
+//   event.preventDefault();
 
-  console.log("comment hit 2")
+//   console.log("comment hit 2")
 
-  const mess_content = document.querySelector('.blog-comment').value.trim();
-  const blog_id = document.querySelector('.blog-num').value.trim();
+//   const mess_content = document.querySelector('.blog-comment').value.trim();
+//   const blog_id = document.querySelector('.blog-num').value.trim();
 
-  console.log(blog_id)
-  console.log(mess_content)
+//   console.log(blog_id)
+//   console.log(mess_content)
 
-  if (mess_content && blog_id) {
+//   if (mess_content && blog_id) {
 
-    const response = await fetch(`/api/comments`, {
-      method: 'POST',
-      body: JSON.stringify({ mess_content, blog_id }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    console.log(response)
-    if (response.ok) {
+//     const response = await fetch(`/api/comments`, {
+//       method: 'POST',
+//       body: JSON.stringify({ mess_content, blog_id }),
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     });
+//     console.log(response)
+//     if (response.ok) {
 
-      document.location.replace('/');
-    } else {
-      alert('Failed to post comment');
-    }
-  }
-}
+//       document.location.replace('/');
+//     } else {
+//       alert('Failed to post comment');
+//     }
+//   }
+// }
 
-document
-  .querySelector('.comment-form')
-  .addEventListener('submit', newCommentFunc);
+// document
+//   .querySelector('.comment-form')
+//   .addEventListener('submit', newCommentFunc);
